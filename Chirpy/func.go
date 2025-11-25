c62b68a5cd81b45fd2900cfd596f2bdb6b1688aa
Dada Oluwatimileyin <oluwatimileyin962@gmail.com>
2025-11-25 04:35:26 +0100

    Added and help function

--- Chirpy/func.go
+++ Chirpy/func.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"log"
 	"net/http"
 	"strings"
+	"fmt"
 )
 
 // called on internal server error for application Json
@@ -54,4 +56,26 @@ func cutAndJoin(subStr, str string) string {
 	count := strings.Count(str, subStr)
 	// replace substr count times with **** in str
 	return strings.Replace(str, subStr, "****", count)
+}
+
+
+
+func respondWithJSON(w http.ResponseWriter, code int, buf bytes.Buffer, payload interface{}) {
+	jsonData, err := json.Marshal(payload)
+	if err != nil {
+		log.Println(err.Error())
+		w.WriteHeader(500)
+		return
+	}
+	buf.Reset()
+	// convert jsonData to string from byte, pass it to a function that returns a string
+	// convert the returned string to byte and svave in jsonData 
+	jsonData = []byte(profaneFUnc(string(jsonData)))
+	buf.Write(jsonData)
+	w.Header().Set("Content-Length", fmt.Sprintf("%d", buf.Len()))
+	
+	w.Header().Set("content-type", "application/json")
+	w.WriteHeader(code)
+	w.Write(buf.Bytes())
+
 }
